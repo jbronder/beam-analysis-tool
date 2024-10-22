e7490494494ab78e06d1bc529e2a9620833277f5
James Bronder <36022278+jbronder@users.noreply.github.com>
2024-10-22 12:19:31 -0700

    style: applied clang-format to file

--- src/util/beamLoadCasesIO.js
+++ src/util/beamLoadCasesIO.js
@@ -5,65 +5,65 @@
 
 /**
  * Returns input load labels and names to populate user input table.
- * @param {string} blcCase 
+ * @param {string} blcCase
  * @return {Object[]}
  */
 function getInputs(blcCase) {
   let specificInputs = [];
 
   switch (blcCase) {
-    case '1':
-    case '12':
-    case '15':
-    case '23':
-      specificInputs = [
-        {
-          description: 'Uniform Load, w (k/ft): ',
-          name: 'load',
-        },
-        {
-          description: 'Length, L (ft): ',
-          name: 'length',
-        }
-      ];
-      break;
-    case '5':
-      specificInputs = [
-        {
-          description: 'Max Uniform Load, w (k/ft): ',
-          name: 'load',
-        },
-        {
-          description: 'Length, L (ft): ',
-          name: 'length',
-        }
-      ];
-      break;
-    case '7':
-    case '13':
-    case '24':
-      specificInputs = [
-        {
-          description: 'Concentrated Load, P (k): ',
-          name: 'load',
-        },
-        {
-          description: 'Length, L (ft): ',
-          name: 'length',
-        }
-      ];
-      break;
-    default:
-      specificInputs = [
-        {
-          description: 'Load: ',
-          name: 'load',
-        },
-        {
-          description: 'Length: ',
-          name: 'length',
-        }
-      ];
+  case '1':
+  case '12':
+  case '15':
+  case '23':
+    specificInputs = [
+      {
+        description : 'Uniform Load, w (k/ft): ',
+        name : 'load',
+      },
+      {
+        description : 'Length, L (ft): ',
+        name : 'length',
+      }
+    ];
+    break;
+  case '5':
+    specificInputs = [
+      {
+        description : 'Max Uniform Load, w (k/ft): ',
+        name : 'load',
+      },
+      {
+        description : 'Length, L (ft): ',
+        name : 'length',
+      }
+    ];
+    break;
+  case '7':
+  case '13':
+  case '24':
+    specificInputs = [
+      {
+        description : 'Concentrated Load, P (k): ',
+        name : 'load',
+      },
+      {
+        description : 'Length, L (ft): ',
+        name : 'length',
+      }
+    ];
+    break;
+  default:
+    specificInputs = [
+      {
+        description : 'Load: ',
+        name : 'load',
+      },
+      {
+        description : 'Length: ',
+        name : 'length',
+      }
+    ];
   }
   return specificInputs;
 }
